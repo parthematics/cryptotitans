1dbbee175d67eae1b895cc6f262d46ace81be87a
Parth Chopra <parthematics@gmail.com>
2018-08-15 14:09:30 -0700

    finished titanmaker.sol

--- titanmaker.sol
+++ titanmaker.sol
@@ -27,5 +27,23 @@ contract TitanMaker is Ownable {
     mapping (uint => address) public titanToPlayer;
     mapping (address => uint) playerToTitans;
     
+    function _createTitan(string _name, uint _featureDNA) internal {
+        uint id = titans.push(Titan(_name, _featureDNA, 1, uint32(now + waitPeriod), 0, 0)) - 1;
+        titanToPlayer[id] = msg.sender;
+        playerToTitans[msg.sender]++;
+        emit NewTitan(id, _name, _featureDNA);
+    }
+    
+    function _generateTitanFeatures(string _str) private view returns (uint) {
+        uint randomFeatures = uint(keccak256(abi.encodePacked(_str)));
+        return randomFeatures % featuresMod;
+    }
+    
+    function createFirstRandomTitan(string _name) public {
+        require(playerToTitans[msg.sender] == 0);
+        uint featuresHash = _generateTitanFeatures(_name);
+        featuresHash = featuresHash - featuresHash % 100;
+        _createTitan(_name, featuresHash);
+    }
     
 }
